57075198650619b4ebdde6591b3b88dd3bfd001b
Pascal Welsch <pascal.welsch@gmail.com>
2018-05-15 19:01:33 +0200

    Add comments

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
 
+echo "Installing Flutter Wrapper..."
+
+# Download latest flutterw version
 curl -O "https://raw.githubusercontent.com/passsy/flutter_wrapper/master/flutterw"
+
+# make it executable
 chmod 755 flutterw
+
+# add it to git
+git add flutterw
+
+# initialize flutter for first run
 ./flutterw
+
+echo "Flutter Wrapper installed, run your app with\n\t./flutterw run"
