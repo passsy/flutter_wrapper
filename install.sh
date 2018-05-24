97a0016cca4aaa67ddc120975690be5015fdb144
Pascal Welsch <pascal.welsch@gmail.com>
2018-05-24 03:02:23 +0200

    Add —tag option to specify a wrapper version
    short -t

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-printf "\nInstalling Flutter Wrapper\n\n"
-
 # Verify flutter project is a git repo
 inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
 if ! [ "$inside_git_repo" ]; then
@@ -15,24 +13,46 @@ if ! [ -f pubspec.yaml ]; then
   printf "Continuing in case this flutter wrapper is used to create a new project. If so continue with './flutterw create .'\n\n"
 fi
 
-# Get latest version from master in git
-LATEST_VERSION=`curl -s "https://raw.githubusercontent.com/passsy/flutter_wrapper/master/version"`
-echo "Latest version is $LATEST_VERSION"
+# Parse arguments
+while [[ $# -gt 0 ]]; do case $1 in
 
+  # version tag which should be used for downloading
+  -t|--tag)
+  shift
+  VERSION_TAG="$1"
+  ;;
+
+  *)
+  echo "Unknown option '$key'"
+  ;;
+  esac
+  shift
+done
+
+if [ -z "$VERSION_TAG" ]; then
+  # Get latest version from master in git
+  VERSION_TAG=`curl -s "https://raw.githubusercontent.com/passsy/flutter_wrapper/master/version"`
+fi
+
+
+printf "Installing Flutter Wrapper $VERSION_TAG\n"
+
+printf "Downloading flutterw\n"
 # Download latest flutterw version
-curl -O "https://raw.githubusercontent.com/passsy/flutter_wrapper/v$LATEST_VERSION/flutterw"
+curl -sO "https://raw.githubusercontent.com/passsy/flutter_wrapper/$VERSION_TAG/flutterw"
 
 # make it executable
 chmod 755 flutterw
 
 # Replace version string in wrapper
-sed -i "s/!VERSION!/$LATEST_VERSION/g" flutterw
+sed -i '' "s/VERSION_PLACEHOLDER/$VERSION_TAG/g" flutterw
 
 # add it to git
 git add flutterw
 
 FLUTTER_DIR_NAME='.flutter'
 
+printf "adding '.flutter' submodule\n"
 # add the flutter submodule
 git submodule add -b master git@github.com:flutter/flutter.git $FLUTTER_DIR_NAME
 
