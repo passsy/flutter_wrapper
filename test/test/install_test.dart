2b7ddc8ee9abe7f59a3f35088d0c18144e0cdd61
Pascal Welsch <pascal@welsch.dev>
2019-11-17 00:27:20 +0100

    Add first tests

---
+++ test/test/install_test.dart
@@ -0,0 +1,88 @@
+import 'dart:io';
+
+import 'package:process_run/cmd_run.dart' as script;
+import 'package:process_run/shell_run.dart' as shell;
+import 'package:test/test.dart';
+
+void main() {
+  group('install', () {
+    test('report missing git', () async {
+      final dir = Directory.systemTemp.createTempSync();
+      addTearDown(() {
+        dir.deleteSync(recursive: true);
+      });
+      final result = await script.run(
+        File("../install.sh").absolute.path,
+        const <String>[],
+        workingDirectory: dir.absolute.path,
+      );
+      expect(result.stdout,
+          contains("Not a git repository, to fix this run: git init"));
+    });
+
+    group("install in git root", () {
+      Directory appDir;
+
+      setUpAll(() async {
+        final dir = Directory.systemTemp.createTempSync();
+        addTearDown(() {
+          dir.deleteSync(recursive: true);
+        });
+        appDir = Directory("${dir.absolute.path}/myApp/");
+
+        await shell.run(
+          """
+          mkdir myApp
+          cd myApp
+          git init
+          """,
+          workingDirectory: dir.absolute.path,
+        );
+
+        await script.run(
+          File("../install.sh").absolute.path,
+          const <String>[],
+          workingDirectory: appDir.absolute.path,
+          verbose: true,
+        );
+      });
+
+      test('flutterw was downloaded', () async {
+        expect(File("${appDir.path}/flutterw").existsSync(), isTrue);
+      });
+      test('flutterw is executable', () async {
+        final flutterw = File("${appDir.path}/flutterw");
+        final result = await Process.run("stat", [flutterw.absolute.path]);
+        final String out = result.stdout as String;
+        expect(out, contains("-rwxr-xr-x"));
+      });
+      test('created .flutter submodule', () async {
+        expect(Directory("${appDir.path}/.flutter").existsSync(), isTrue);
+      });
+      test('downloaded dart tools', () async {
+        expect(
+            File("${appDir.path}/.flutter/bin/cache/dart-sdk/bin/dart")
+                .existsSync(),
+            isTrue);
+        expect(
+            File("${appDir.path}/.flutter/bin/cache/dart-sdk/bin/dartanalyzer")
+                .existsSync(),
+            isTrue);
+        expect(
+            File("${appDir.path}/.flutter/bin/cache/dart-sdk/bin/dartfmt")
+                .existsSync(),
+            isTrue);
+        expect(
+            File("${appDir.path}/.flutter/bin/cache/dart-sdk/bin/pub")
+                .existsSync(),
+            isTrue);
+      });
+      test('flutterw contains version', () async {
+        final flutterw = File("${appDir.path}/flutterw");
+        final text = flutterw.readAsStringSync();
+        expect(text, isNot(contains("VERSION_PLACEHOLDER")));
+        expect(text, isNot(contains("DATE_PLACEHOLDER")));
+      });
+    });
+  });
+}
